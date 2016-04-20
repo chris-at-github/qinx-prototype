5fa25f614a8bedc22b0bed48c39673afbffb1001
Christian Pschorr <c.pschorr@datamints.com>
2016-04-20 09:41:42 +0200

    Add composer to project

--- public/a4.php
+++ public/a4.php
@@ -3,7 +3,7 @@
 define('A4_PATH', realpath(__DIR__ . '/..'));
 define('A4_VERSION', '0.0.1');
 
-require(A4_PATH . '/a4/application.php');
+require(A4_PATH . '/A4/Application.php');
 
-$g4 = new Application();
-$g4->execute($_REQUEST['page']);
+$a4 = new Application();
+$a4->execute($_REQUEST['page']);
