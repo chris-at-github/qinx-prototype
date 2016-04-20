9e4a99518515b269888cb01f1d1cf4be668605e1
Christian Pschorr <c.pschorr@datamints.com>
2016-04-20 09:55:56 +0200

    Change directory names to small caps

--- public/a4.php
+++ public/a4.php
@@ -3,7 +3,7 @@
 define('A4_PATH', realpath(__DIR__ . '/..'));
 define('A4_VERSION', '0.0.1');
 
-require(A4_PATH . '/Vendor/autoload.php');
+require(A4_PATH . '/vendor/autoload.php');
 
 $a4 = new \A4\Application();
 $a4->execute($_REQUEST['page']);
