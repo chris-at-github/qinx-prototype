e37efccd70260f59566ea6322e59b34ec0b2826d
Christian Pschorr <pschorr.christian@gmail.com>
2016-04-20 21:39:05 +0200

    basic renderer parent class implementation

--- a4/Renderer/Renderer.php
+++ a4/Renderer/Renderer.php
@@ -1,13 +1,69 @@
 <?php
 namespace A4\Renderer;
 
+/**
+ * Class Renderer
+ * @package A4\Renderer
+ */
 class Renderer {
+
+	/**
+	 * Rendering source
+	 *
+	 * @var string $sSource
+	 */
+	protected $sSource;
 
 	/**
 	 * Instance of current application
 	 *
-	 * @var \A4\Application $application
+	 * @var \A4\Application $oApplication
 	 */
-	protected $application;
+	protected $oApplication;
 
+	/**
+	 * Getter for renderer source
+	 *
+	 * @return string
+	 */
+	public function getSource() {
+		return $this->sSource;
+	}
+
+	/**
+	 * Setter for renderer source
+	 *
+	 * @param string $sSource
+	 */
+	public function setSource($sSource) {
+		$this->sSource = $sSource;
+	}
+
+	/**
+	 * Setter for application
+	 *
+	 * @return \A4\Application
+	 */
+	public function getApplication() {
+		return $this->oApplication;
+	}
+
+	/**
+	 * Getter for application
+	 *
+	 * @param \A4\Application $oApplication
+	 */
+	public function setApplication(\A4\Application $oApplication) {
+		$this->oApplication = $oApplication;
+	}
+
+	/**
+	 * Renderer constructor
+	 *
+	 * @param string|null $sSource
+	 * @param \A4\Application|null $oApplication
+	 */
+	public function __construct($sSource = null, \A4\Application $oApplication = null) {
+
+	}
 }
