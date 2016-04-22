e921f34e72d3e7355b8a5bbe3d6eab7edf649887
Christian Pschorr <pschorr.christian@gmail.com>
2016-04-22 06:33:54 +0200

    Layout for world map and tiles

--- public/world/index.php
+++ public/world/index.php
@@ -1,7 +1,23 @@
 <?php $partial->render('Header'); ?>
 
-	<div id="map" class="placeholder">
-		<div class="placeholder--title">Mp</div>
+	<div class="row world-map">
+		<div class="col-sm-10">
+			<div id="world-map--container" class="placeholder">
+				<span class="placeholder--title">World Map</span>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<a href="#" class="placeholder world-map--tile">
+				<span class="placeholder--title">Colony #01</span>
+			</a>
+			<a href="#" class="placeholder world-map--tile">
+				<span class="placeholder--title">Resource #01</span>
+			</a>
+			<a href="#" class="placeholder world-map--tile">
+				<span class="placeholder--title">Resource #02</span>
+			</a>
+		</div>
 	</div>
+
 
 <?php $partial->render('Footer'); ?>
