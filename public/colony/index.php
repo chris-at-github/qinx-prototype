67f120794f51de9729a58b029f60d39d0295871c
Christian Pschorr <pschorr.christian@gmail.com>
2016-04-22 06:39:22 +0200

    Create colony page

---
+++ public/colony/index.php
@@ -0,0 +1,14 @@
+<?php $partial->render('Header'); ?>
+
+<div class="row">
+	<div class="col-sm-8">
+
+		<div class="placeholder">
+			<div class="placeholder--title">Building</div>
+		</div>
+
+	</div>
+</div>
+
+
+<?php $partial->render('Footer'); ?>
