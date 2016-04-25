fbf21e98bfa78d84423f6e5b226d7ddc8e580d18
Christian Pschorr <pschorr.christian@gmail.com>
2016-04-25 07:16:49 +0200

    include bootstrap tabs and move world items (trade, units, ...) to tab items

--- public/world/index.php
+++ public/world/index.php
@@ -1,47 +1,71 @@
 <?php $partial->render('Header'); ?>
 
-<div class="container">
-	<div class="container--header">World Map</div>
+<div class="row">
+	<div class="col-sm-10">
+		<div class="container">
+			<div class="container--header">World Map</div>
 
-	<div class="row world-map">
-		<div class="col-sm-10">
-			<div id="world-map--container" class="placeholder">
-				<span class="placeholder--title">World Map</span>
+			<div class="row world-map">
+				<div class="col-sm-10">
+					<div id="world-map--container" class="placeholder">
+						<span class="placeholder--title">World Map</span>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<a href="<?php echo \A4\Helper\UriHelper::get('colony'); ?>" class="placeholder world-map--tile">
+						<span class="placeholder--title">Colony #01</span>
+					</a>
+					<a href="#" class="placeholder world-map--tile">
+						<span class="placeholder--title">Resource #01</span>
+					</a>
+					<a href="#" class="placeholder world-map--tile">
+						<span class="placeholder--title">Resource #02</span>
+					</a>
+				</div>
 			</div>
-		</div>
-		<div class="col-sm-6">
-			<a href="<?php echo \A4\Helper\UriHelper::get('colony'); ?>" class="placeholder world-map--tile">
-				<span class="placeholder--title">Colony #01</span>
-			</a>
-			<a href="#" class="placeholder world-map--tile">
-				<span class="placeholder--title">Resource #01</span>
-			</a>
-			<a href="#" class="placeholder world-map--tile">
-				<span class="placeholder--title">Resource #02</span>
-			</a>
 		</div>
 	</div>
-</div>
+	<div class="col-sm-6">
+		<div class="container tabs">
 
-<div class="container">
-	<div class="container--header">World</div>
+			<nav class="tabs--navigation">
+				<ul>
+					<li class="active">
+						<a href="#units" data-toggle="tab">Units</a>
+					</li>
+					<li>
+						<a href="#trade" data-toggle="tab">Trade</a>
+					</li>
+					<li>
+						<a href="#expeditions" data-toggle="tab">Expeditions</a>
+					</li>
+					<li>
+						<a href="#players" data-toggle="tab">Players</a>
+					</li>
+				</ul>
+			</nav>
 
-	<div class="row">
-		<div class="col-sm-4">
-			<div class="placeholder">
-				<div class="placeholder--title">Units</div>
-			</div>
-		</div>
-
-		<div class="col-sm-4">
-			<div class="placeholder">
-				<div class="placeholder--title">Trade</div>
-			</div>
-		</div>
-
-		<div class="col-sm-4">
-			<div class="placeholder">
-				<div class="placeholder--title">Players</div>
+			<div class="tabs--content">
+				<div class="tabs--item active" id="units">
+					<div class="placeholder">
+						<div class="placeholder--title">Units</div>
+					</div>
+				</div>
+				<div class="tabs--item" id="trade">
+					<div class="placeholder">
+						<div class="placeholder--title">Trade</div>
+					</div>
+				</div>
+				<div class="tabs--item" id="expeditions">
+					<div class="placeholder">
+						<div class="placeholder--title">Expeditions</div>
+					</div>
+				</div>
+				<div class="tabs--item" id="players">
+					<div class="placeholder">
+						<div class="placeholder--title">Players</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- source/js/a4.js
+++ source/js/a4.js
@@ -1,2 +1,26 @@
 require('./vendor/bootstrap/dropdown.js');
+require('./vendor/bootstrap/tabs.js');
 require('./vendor/vodkabears/remodal.js');
+
+$(function() {
+
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Tabs
+	$('.tabs .tabs--navigation a').click(function(e) {
+		e.preventDefault();
+
+		var tab = $(this);
+		var text = tab.text();
+
+		// Dropdown Toggle mit aktuellen Wert befuellen
+		$('.dropdown-toggle', tab.closest('.dropdown')).text(text);
+
+		// Active Klasse bei allen vorherigen entfernen
+		$('li', tab.closest('ul')).removeClass('active');
+
+		// Tab aktivieren
+		tab.tab('show');
+	});
+
+});
