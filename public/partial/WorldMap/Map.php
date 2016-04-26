4cd2f72025d25f8d9e6744275e3849e4d7de7950
Christian Pschorr <pschorr.christian@gmail.com>
2016-04-26 05:53:45 +0200

    Add a tile container element with overflow hidden to add more tiles without break

--- public/partial/WorldMap/Map.php
+++ public/partial/WorldMap/Map.php
@@ -1,20 +1,17 @@
 <div class="world-map">
-	<?php for($y = 0; $y <= 3; $y++) {?>
-		<div class="world-map--tiles">
-			<?php for($x = 0; $x <= 4; $x++) {?>
-				<div class="world-map--tile">
+	<div class="world-map--tile-container">
+		<?php for($y = 0; $y <= 3; $y++) { ?>
+			<div class="world-map--tiles">
+				<?php for($x = 0; $x <= 8; $x++) { ?>
+					<div class="world-map--tile">
 
-					<?php if($y === 2 && $x === 3) { ?>
-						<a href="#" class="world-map--tile-action">Colony #01</a>
-						<a href="#" class="world-map--tile-action">Unit #01</a>
-						<a href="#" class="world-map--tile-action">Unit #02</a>
-						<a href="#" class="world-map--tile-action">Resource #01</a>
-						<a href="#" class="world-map--tile-action">Resource #03</a>
-						<a href="#" class="world-map--tile-action">Resource #17</a>
-					<?php } ?>
+						<?php if($y === 2 && $x === 3) { ?>
+							<?php \A4\Factory::get(\A4\Helper\LinkHelper::class)->render('colony', 'Colony #01', ['class' => 'world-map--tile-action']) ?>
+						<?php } ?>
 
-				</div>
-			<?php } ?>
-		</div>
-	<?php } ?>
+					</div>
+				<?php } ?>
+			</div>
+		<?php } ?>
+	</div>
 </div>
