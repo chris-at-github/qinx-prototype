f649232f0185225a21d51a1caa05f0355d1ba2b5
Christian Pschorr <pschorr.christian@gmail.com>
2016-04-26 07:14:13 +0200

    Add colony pager

--- public/colony/index.php
+++ public/colony/index.php
@@ -1,5 +1,15 @@
 <?php $partial->render('Header'); ?>
 
+	<div class="colony--header">
+		<nav class="colony--pager pager pager-center">
+			<ul>
+				<li class="colony--pager-previous pager--previous"><a href="#01"><span class="icon icon-chevron-left"></span></a></li>
+				<li class="colony--pager-current pager--current">Colony #01</li>
+				<li class="colony--pager-next pager--next"><a href="#01"><span class="icon icon-chevron-right"></span></a></li>
+			</ul>
+		</nav>
+	</div>
+
 <div class="container">
 	<header class="container--header">Colony</header>
 
@@ -19,10 +29,6 @@
 
 			<div class="placeholder">
 				<div class="placeholder--title">Worker</div>
-			</div>
-
-			<div class="placeholder">
-				<div class="placeholder--title">Expeditions</div>
 			</div>
 
 		</div>
