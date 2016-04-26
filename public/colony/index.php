2e22efb28fb779283ed950104d1fc68311cc2d9e
Christian Pschorr <c.pschorr@datamints.com>
2016-04-26 14:01:43 +0200

    Create html / css for resource items

--- public/colony/index.php
+++ public/colony/index.php
@@ -10,29 +10,68 @@
 		</nav>
 	</div>
 
-<div class="container">
-	<header class="container--header">Colony</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8">
 
-	<div class="row">
-		<div class="col-sm-8">
+				<div class="placeholder">
+					<div class="placeholder--title">Worker</div>
+				</div>
 
-			<div class="placeholder">
-				<div class="placeholder--title">Buildings</div>
 			</div>
+			<div class="col-sm-8">
 
-			<div class="placeholder">
-				<div class="placeholder--title">Resources</div>
+				<header>Resources</header>
+
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="resource">
+							<div class="resource--unit">789</div>
+							<div class="resource--header">
+								<div class="resource--header-name">WoodResource</div>
+								<div class="resource--header-namespace">\A4\Resource\WoodResource</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="resource">
+							<div class="resource--unit">156</div>
+							<div class="resource--header">
+								<div class="resource--header-name">StoneResource</div>
+								<div class="resource--header-namespace">\A4\Resource\StoneResource</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="resource">
+							<div class="resource--unit">341</div>
+							<div class="resource--header">
+								<div class="resource--header-name">WaterResource</div>
+								<div class="resource--header-namespace">\A4\Resource\WaterResource</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="resource">
+							<div class="resource--unit">19</div>
+							<div class="resource--header">
+								<div class="resource--header-name">LeafResource</div>
+								<div class="resource--header-namespace">\A4\Resource\LeafResource</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
-
-		</div>
-		<div class="col-sm-8">
-
-			<div class="placeholder">
-				<div class="placeholder--title">Worker</div>
-			</div>
-
 		</div>
 	</div>
-</div>
+
+	<div class="container">
+		<header class="container--header">Colony</header>
+
+		<div class="placeholder">
+			<div class="placeholder--title">Buildings</div>
+		</div>
+	</div>
 
 <?php $partial->render('Footer'); ?>
